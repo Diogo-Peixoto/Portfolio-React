091bcac0476b66cdf7e98b30580d17ab28429306
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2022-11-19 15:40:59 -0300

    componet footer complet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import { About } from "./components/About"
 import { Contact } from "./components/Contact"
+import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { Home } from "./components/Home"
 import { Qualification } from "./components/Qualification"
@@ -18,6 +19,7 @@ function App() {
         <Skills />
         <Qualification />
         <Contact />
+        <Footer />
       </main>
     </>
   )
